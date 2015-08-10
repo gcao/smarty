ec939ff898fa0ec02ecca1327b3306ef531d95e0
Lisa Venezia <lvenezia@vonage.com>
2015-08-10 13:36:09 -0400

    Install Jira scripts
    Added jira-issues.coffee and jira.coffee.

--- hipchat_startup.sh
+++ hipchat_startup.sh
@@ -4,5 +4,11 @@ export EXPRESS_PORT=9090
 export HUBOT_HIPCHAT_JID=""
 export HUBOT_HIPCHAT_PASSWORD=""
 export HUBOT_HIPCHAT_ROOMS=""
+export HUBOT_JIRA_LOOKUP_USERNAME="proteus_user"
+export HUBOT_JIRA_LOOKUP_PASSWORD="proteus"
+export HUBOT_JIRA_LOOKUP_URL="https://jiradev.snap.vonagenetworks.net:8443/jiraDev/"
+export HUBOT_JIRA_URL="https://jiradev.snap.vonagenetworks.net:8443/jiraDev/"
+export HUBOT_JIRA_USER="proteus_user"
+export HUBOT_JIRA_PASSWORD="proteus"
 
 bin/hubot --adapter hipchat
