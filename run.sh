3eaa450196d67b775d9186e4b9eca04469dbf936
Guoliang Cao <gcao99@gmail.com>
2016-01-18 08:31:21 -0500

    Switch to slack

--- run.sh
+++ run.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-
-# while true; do ./hipchat_startup.sh; sleep 1; done
-# Passwords and stuff shouldn't be in source control.. good thing this is private!
+# while true; do ./run.sh; sleep 1; done
 
 export EXPRESS_PORT=${EXPRESS_PORT:-9090}
-
-export HUBOT_HIPCHAT_JID="30164_3213923@chat.hipchat.com"
-#export HUBOT_HIPCHAT_PASSWORD=""
-export HUBOT_HIPCHAT_ROOMS="30164_room_99@conf.hipchat.com,30164_room_98@conf.hipchat.com"
 
 export HUBOT_JIRA_LOOKUP_USERNAME="proteus_user"
 #export HUBOT_JIRA_LOOKUP_PASSWORD=""
@@ -17,9 +11,6 @@ export HUBOT_JIRA_USERNAME="proteus_user"
 #export HUBOT_JIRA_PASSWORD=""
 export HUBOT_JIRA_USE_V2="true"
 
-#export HUBOT_STANDUP_PREPEND="(standup)"
-#export HUBOT_STANDUP_ROOM="296829_onboarding@conf.hipchat.com"
-
 export HUBOT_JENKINS_URL=http://subscribe-vm-06.dloco.s.vonagenetworks.net:8080/
 
 export AMQP_SERVER=${AMQP_SERVER:-subscribe-vm-06.dloco.s.vonagenetworks.net}
@@ -27,4 +18,4 @@ export AMQP_USERNAME=${AMQP_USERNAME:-guest}
 export AMQP_PASSWORD=${AMQP_PASSWORD:-guest}
 export AMQP_URL=amqp://$AMQP_USERNAME:$AMQP_PASSWORD@$AMQP_SERVER
 
-bin/hubot --adapter hipchat
+bin/hubot --adapter slack
